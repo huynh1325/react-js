961f438ff58648972976a5624d97a30518fe7bae
huynh <huynhnguyen120202@gmail.com>
2024-09-26 22:19:42 +0700

    React Context API

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -1,10 +1,12 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Button, Form, Input, notification } from 'antd';
 import { loginApi } from '../util/api';
 import { useNavigate } from 'react-router-dom';
+import { AuthContext } from '../components/context/auth.context';
 
 const LoginPage = () => {
     const navigate = useNavigate();
+    const { setAuth } = useContext(AuthContext);
     const onFinish = async (values) => {
         const {email, password} = values;
         const res = await loginApi(email, password);
@@ -13,6 +15,13 @@ const LoginPage = () => {
             notification.success({
                 message: "LOGIN USER",
                 description: "Success"
+            });
+            setAuth({
+                isAuthenticated: true,
+                user: {
+                    email: res?.user?.email ?? "",
+                    name: res?.user?.name ?? ""
+                }
             })
             navigate("/");
         } else {
